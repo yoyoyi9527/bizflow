5778f2c947554e381505e96b11905bdf39bc77ec
yoyoyi9527 <87069088@qq.com>
2026-04-18 18:01:43 +0800

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,20 @@
 import "./globals.css";
+import type { Metadata } from "next"; // 增加类型定义
+
+export const metadata: Metadata = {
+  title: "个人作品集 | Bizflow",
+  description: "系统经验设计师作品展示",
+};
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="zh">
+      {/* 这里的 antialiased 会让你的字体看起来和设计稿一样细腻 */}
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
